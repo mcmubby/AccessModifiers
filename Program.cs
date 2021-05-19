d28e39f39062c46b653892b0a299af76431df2e8
Mubarak <12859241+mcmubby@users.noreply.github.com>
2021-05-19 20:06:36 +0100

    eoc

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace AccessModifiers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 1. Private 
+            // 2. Protected
+            // 3. Internal
+            // 4. Protected Internal
+            // 5. Public
+
+            // Private members are available only within the containing type.
+
+            // Public members are available anywhere.
+
+            // Protected member are available within containing type and to the types
+            // that derive from the containing type.
+
+            // Internal members are not accessible outside the assembly in which they
+            // are contained.
+
+            // Protected Internal is available to all members in the assembly and to 
+            // any member that inherits from the type outside the assembly.
+
+            // Only public and internal can be assigned to types(class,struct,interface,delegates).
+            // If you don't specify access modifiers for types, it will have internal as the
+            // default.
+            // Type members can have all access modifiers.
+            // If you don't specify access modifiers for type members, it will be assigned 
+            // private by default.
         }
     }
 }
